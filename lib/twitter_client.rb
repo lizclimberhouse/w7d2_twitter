71278efd643daca3f073ab42c6b0096b8d32cd98
Liz House <lizehouse@gmail.com>
2018-03-20 10:26:49 -0600

    initial commit

---
+++ lib/twitter_client.rb
@@ -0,0 +1,16 @@
+class TwitterClient
+
+  def self.home_timeline
+    client.home_timeline
+  end
+
+  private
+    def self.client
+      Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['CONSUMER_KEY']
+      config.consumer_secret = ENV['CONSUMER_SECRET']
+      config.access_token = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+  end
+end
